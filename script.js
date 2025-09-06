3e4ab7193355132037962f2de2cf080ba4f2686d
Skotttie <deniztabak60@gmail.com>
2025-09-07 00:57:11 +0200

    parallax added & slider bug fix

--- script.js
+++ script.js
@@ -11,31 +11,45 @@ const observer = new IntersectionObserver(entries => {
 
 cards.forEach(card => observer.observe(card));
 
-// Header animation
+// Header (Logo) animation
 const logo = document.querySelector('.logo img');
-const maxScroll = 300; // scrollhöhe für Logo-Movement
+const maxScroll = 300; // Scrollhöhe für Logo-Movement
+
+// Startwert einmalig beim Laden setzen
+let startLeft = logo.getBoundingClientRect().left; // aktueller Abstand zum linken Fensterrand
+const endLeft = (window.innerWidth / 2) - (logo.offsetWidth / 2);
 
 window.addEventListener('scroll', () => {
     const scrollY = window.scrollY;
 
-    // Logo bewegt sich von links nach Mitte
-    const startLeft = 0; // links
-    const endLeft = window.innerWidth / 2 - logo.offsetWidth / 2;
     const currentLeft = startLeft + (endLeft - startLeft) * Math.min(scrollY / maxScroll, 1);
+
     logo.style.position = 'absolute';
     logo.style.left = `${currentLeft}px`;
 });
 
 // Slideshow
+window.addEventListener("load", () => {
+    document.querySelector('.slideshow-track').style.animationPlayState = 'running';
+});
+
 let index = 0;
 const slides = document.querySelectorAll('.slide');
 const slideshow = document.querySelector('.slideshow-container');
 
-function nextSlide() {
-    index = (index + 1) % slides.length; // Schleife
-    slideshow.style.transform = `translateX(-${index * 100}%)`;
-}
-setInterval(nextSlide, 3000); // alle 3 Sekunden wechseln
+// Slideshow - Spotify übergang
+window.addEventListener('scroll', function() {
+    const scrollY = window.scrollY;
+
+    const slider = document.querySelector('.slideshow-container');
+    const spotify = document.querySelector('.spotify-wrapper');
+
+    // Slider bewegt sich langsamer nach oben
+    slider.style.transform = `translateY(${scrollY * 0.3}px)`;
+
+    // Spotify bewegt sich schneller
+    spotify.style.transform = `translateY(${scrollY * 0.1}px)`;
+});
 
 // Spotify Logo animation
 document.addEventListener("DOMContentLoaded", () => {
